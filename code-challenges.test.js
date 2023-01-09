3d91296d16d24409515e51099a2ca879c5325053
msawhitney <msawhit3@gmail.com>
2023-01-09 12:39:35 -0800

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,6 +12,19 @@
 
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 
+// Pseudocode 
+
+// Input: array
+// Output: a shuffled subset of the array minus the first item in the original array 
+
+// Process: Remove the first item in the array (use splice or shift)  and make a variable that holds the resulting array.
+// Randomize mixing up the remaining array. Apparently there is a shuffle method.
+// Or try this: const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const shuffledArray = array.sort((a, b) => 0.5 - Math.random());
+// not totally random. Try this:
+// https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj#:~:text=The%20first%20and%20simplest%20way,)%20%3D%3E%200.5%20%2D%20Math.
+
+
 // a) Create a test with an expect statement using the variable provided.
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
@@ -25,22 +38,110 @@ const colors2 = [
   "aquamarine",
   "saffron"
 ]
+
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+describe ('shuffleArray', () => {
+  it ('takes in an array, removes the first item from the array and shuffles the remaining content', () => {
+expect(shuffleArray(colors1)).toContain("yellow") 
+expect(shuffleArray(colors1)).toContain("blue")
+expect(shuffleArray(colors1)).toContain("pink")
+expect(shuffleArray(colors1)).toContain("green")
+expect(shuffleArray(colors1)).not.toEqual(colors1)
+expect(shuffleArray(colors2)).toContain("periwinkle") 
+expect(shuffleArray(colors2)).toContain("indigo")
+expect(shuffleArray(colors2)).toContain("ochre")
+expect(shuffleArray(colors2)).toContain("aquamarine")
+expect(shuffleArray(colors2)).toContain("saffron")
+expect(shuffleArray(colors2)).not.toEqual(colors2)   
+  })
+})
+
+// RED Fail ---
+// FAIL  ./code-challenges.test.js
+// ● shuffleArray › takes in an array, removes the first item from the array and shuffles the remaining content
+//   ReferenceError: shuffleArray is not defined
+
+
+const shuffleArray = (array) => {
+  //.shift will remove the first element of the array
+  array.shift();
+  // The shuffle used here is the Fisher-Yates shuffle
+  // A loop is set up to iterate over the elements of the array
+  // the iteration starts at the last element of the array (array.length - 1)
+  // it continues as long as i is greater than zero 
+  // And i will be decremented by 1 on each iteration (i--)
+  for (let i = array.length - 1; i > 0; i--) {
+      //apparently, this generates a random number between 0 and i (inclusive)
+      //and assigns it to the j variable
+      const j = Math.floor(Math.random() * (i + 1));
+      //this will destructure the arrays and swap elements at the i and j indexes
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+      // array.sort(() => Math.random() - 0.5); <-- I tried this method. It works, too. I'm not sure which is the most random shuffle method.
+  return array
+}
+
 // b) Create the function that makes the test pass.
+// JEST TEST - Passed the first time. I'm not sure how to write the test to take in each succeeding shift and then shuffle. It is failing now because the function is taking out the first element each time and test expects that element to be in. 
+
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+// Pseudocode
+// Input: object with upvote/downvote values.
+// Output: number that is the difference of the first value in an object and the second value in the object. 
+
+// Process: create a function called tallyVotes that accesses two values in an object 
+// votes is the parameter
+// return votes.upVotes minus votes.downVotes;
+// subtracts the second value from the first value. Have it return the difference.
+
 
 const votes1 = { upVotes: 13, downVotes: 2 }
 // Expected output: 11
 const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
 
+
+describe ('tallyVotes', (votes) => {
+  it ('takes in an object that contains up votes and down votes and returns the end tally', () => {
+expect(tallyVotes(votes1)).toEqual(11) 
+expect(tallyVotes(votes2)).toEqual(-31)   
+  })
+})
+
+// RED FAIL - 
+// FAIL  ./code-challenges.test.js
+//   ReferenceError: tallyVotes is not defined
+
 // b) Create the function that makes the test pass.
 
+const tallyVotes = (votes) => {
+  return votes.upVotes - votes.downVotes
+}
+
+// GREEN PASS - 
+// PASS  ./code-challenges.test.js
+// tallyVotes
+//   ✓ takes in an object that contains up votes and down votes and returns the end tally (1 ms)
+
+
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
+
+// Pseudocode
+// Input: two arrays 
+// Output: combined array without duplicate values 
+// Process: Apparently there are a few ways to solve this problem, but I'm choosing this one - destructure the arrays into a new set (sets do not allow duplicates) (see link below for source).
+// Create a function called removeDups. 
+// Create a set to store unique values in called uniqValues that takes two arrays as arguments. let uniqValues = new Set(array1, array2). 
+// Iterate through array 1 and and use .add to add each element to the new set. 
+// Then iterate through the second array and add each element to the set the same way. Duplicates will be skipped and not added to a set.
+// Then use .from which creates an array from an array-like object (the set) to create the final array with no duplicates
+
+// Here is where I learned about sets: https://codeburst.io/how-to-merge-arrays-without-duplicates-in-javascript-91c66e7b74cf
 
 // a) Create a test with an expect statement using the variables provided.
 
@@ -48,4 +149,32 @@ const dataArray1 = ["array", "object", "number", "string", "Boolean"]
 const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
 // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
 
+describe('removeDups', () => {
+  it ('should return one array with no duplicate values from two arrays - array1 and array2', () =>  {
+   expect (removeDups (dataArray1, dataArray2)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"]) 
+  });
+});
+
+// RED FAIL:  FAIL  ./code-challenges.test.js
+
 // b) Create the function that makes the test pass.
+
+const removeDups = (array1, array2) => {
+  let uniqValues = new Set()
+  //iterate through array1
+   for (let i = 0; i < array1.length; i++) {
+     //add the values of array1 to the set called uniqValues
+    uniqValues.add(array1[i])
+   }
+   for (let i = 0; i < array2.length; i++) {
+    //iterate through array2
+     //add the values of array2 to the set called uniqValues. No duplicate values will be added, because it is a set (and sets don't allow duplicate values).
+     uniqValues.add(array2[i])
+   }
+    //  return the set as an array using .from to create it
+    //Array should to be capitalized because Array.from is a constructor function used to create the array.
+    return Array.from (uniqValues)
+}
+  
+// GREEN ---PASS  ./code-challenges.test.js
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,42 +8,110 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is object-oriented programming? How is it different than functional programming?
 
-Your answer:
+Your answer: Object-oriented programming is all based on objects and classes. Functions are all inside objects, so they are called methods. All programming revolves around classes and objects. Other programming languages have more to do with functions. JavaScript, for example uses functions that are independent from objects. 
 
-Researched answer:
+Researched answer: (OOP) is programming that is based on data or "objects" as opposed to functions and logic. Objects are self-contained units that consists of both data and code and represent a real-world object or concept. 
+In OOP, objects interact through methods (functions associated with an object). Objects can inherit properties and behavior from parent objects. Here are some key principles of OOP: 
 
-1. What is the difference between a Float and an Integer in Ruby?
+Some differences between OOP and functional programming: 
+Where OOP is based around objects, functional programming is based around the concept of functions which are independent units of programming. Functions take input and produce output. 
+OOP emphasizes the structure of the code and relationships between objects. Functional programming emphasizes evaluating expressions and transforming data. 
+OOP is often used to build large, complex systems by dividing them into smaller reusable units (objects). Functional programming is often use to solve mathematical problems or to write brief, efficient code. 
+Many computer languages use a combination of these methods. 
 
-Your answer:
+1. What is the difference between a Float and an Integer in Ruby? 
 
-Researched answer:
+Your answer: A float contains portions of a number (like the .45 in the number 3.45). Integers are whole numbers (like 3 or 4)
 
-1. Ruby has an implicit return. What does that mean?
+Researched answer: in Ruby, an integer is a whole number, while a float is a number with a decimal point. Float is short for "floating point number." Some operations in Ruby behave differently for floats and integers. An example is division. 10/3 will output 3, where 10.0/3 will output 3.3333333333333335. 
+For storing whole numbers and performing exact math, use integers. For storing decimal numbers and for higher precision, use floats.
 
-Your answer:
+1. Ruby has an implicit return. What does that mean? 
 
-Researched answer:
+Your answer: It means the word return is not required in a function (or method). Implicit means it's implied. 
 
-1. What is a block in Ruby?
+Researched answer: If a return is the last expression in a path of execution in Ruby, there is no need to add the keyword "return." It is there by default. Ruby methods will always return the evaluated result of the last line unless an explicit return comes before it. 
 
-Your answer:
+1. What is a block in Ruby? 
 
-Researched answer:
+Your answer: A block in Ruby is like the portion between curly braces in JavaScript. It starts with a "def' and ends with an "end."
+
+Researched answer: A block is a piece of code that can be passed to a method as an argument. It is defined using "do" and "end", or with curly braces. Blocks are used in Ruby commonly to perform an operation on each element of things such as arrays or hashes. 
 
 1. How do you extract a value in a Ruby hash?
 
-Your answer:
+Your answer: You use the key. 
 
-Researched answer:
+Researched answer: You can use bracket notation. After the hash name, type the key in square brackets. 
+Example: hash = {key1: "value1", key2: "value2}
+value1 = hash[:key1] will return value1. 
+
+Another way to extract a value is to use "fetch." Fetch allows you to specify a default value to return if the key is not found in the hash. Using the above example object, you can use value1 = hash.fetch(:key1, "default")
 
 ## Looking Ahead: Terms for Next Week
 
-1. Class Inheritance:
+1. Class Inheritance:  In Ruby, this is a way to create a new class that is a modified version of an original class. The new class inherits behavior from the original. The class that is inheriting the behavior is called the subclass. The original class is called the superclass. 
 
-2. RSpec:
+The subclass inherits characteristics from the superclass but it can also have characteristics of its own. 
 
-3. CRUD:
+Example: 
 
-4. Test-driven development:
+class Animal
+    def speak
+        puts "Hello!"
+    end
+end
 
-5. HTTP:
+class Dog < Animal
+<!-- //Above, Dog is defined as a subclass of Animal, so now it has access to methods defined in Animal (like speak) and it can have its own methods, (like bark). -->
+
+    def bark
+        puts "Ruff!"
+    end
+end
+
+dog=Dog.new
+dog.speak #prints "Hello!"
+dog.bark #prints "Woof!"
+
+2. RSpec: RSpec stands for Ruby Specification. It is a testing library for Ruby (like Jest is for Javascript.) It is designed to be readable and easy to use. Like Jest, there is a describe section and an it section. 
+
+Here is an example:
+
+describe "A simple calculator" do
+    it "should add numbers" do
+    calculator = Calculator.new
+    result = calculator.add(2,3)
+    expect(result).to eq(5)
+end    
+
+
+3. CRUD: This is an acronym that stands for - Create, Read, Update, Delete. These are the four ways to manipulate data in a database or in persistent storage. 
+Create - create data/Post
+Read - display data/get
+Update - edit data
+Delete - remove data
+
+4. Test-driven development: This is a method of software development where tests are written for a piece of code before the code itself is written (like we do in Jest testing in Javascript). This makes the developer focus on requirements before they write the code. Here is the order that test-driven development takes: 
+
+    a. The test is written.
+
+    b. The test should fail at first, for expected reasons. This means that the code is needed for the test to pass and rules out the possibility that the new test will always pass and is flawed.
+
+    c. Then the simplest amount of code is written that allows the test to pass. If the test fails, code should be rewritten to pass. 
+
+    d. Test is run again and should pass.
+
+    e. Code is refactored as needed to improve design and code readability, while ensuring that the behavior and functionality of the code is preserved.
+
+    f. The process is repeated.
+
+5. HTTP: Stands for Hypertext Transfer Protocol. It is a set of rules for transfering data on the internet. It is used to send and recieve data between web clients and servers on the internet and is based on request and response. Requests go out from individual computers to servers and a response returns from the server. The HTTP verbs (based on requests) are: get, post, put, patch, and delete. 
+Possible esponses from the server can be evaluated based on the following status codes: 
+100 - informational
+200 - success
+300 - redirect
+400 - error on the request side (client side)
+500 - error on the response side (server side)
+
+
